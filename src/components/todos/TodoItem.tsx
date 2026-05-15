5a910ddf9db03711fca95a865ac0cac356a1ece4
NJH <ggpji59105@naver.com>
2026-05-15 14:36:23 +0900

    feat(todo): 수정 기능 추가

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,5 +1,5 @@
 import type { Todo } from '@/stores/todo'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 interface Props {
   todo: Todo
@@ -7,12 +7,26 @@ interface Props {
 
 export default function TodoItem({ todo }: Props) {
   const [isEditMode, setIsEditMode] = useState(false)
+  const [title, setTitle] = useState(todo.title)
+  const inputRef = useRef<HTMLInputElement>(null) //input 요소에 연결된다는 타입 이해할 수 있음
+
+  useEffect(() => {
+    if (isEditMode) {
+      inputRef.current?.focus() //수정 버튼 누르면 자동 포커스
+    }
+  }, [isEditMode])
 
   function onEditMode() {
-    setIsEditMode(true)
+    setIsEditMode(true) //데이터만 수정
+    // setter는 비동기적으로 동작한다. 데이터가 변경됐다고 해서 화면이 바로 변경됨을 보장하지 않는다.
+    // 따라서 아래 포커스 상황이 발생하지 않는다.
+    // useEffect 사용
+    // inputRef.current?.focus() => useEffect 내로 이동
   }
+
   function offEditMode() {
     setIsEditMode(false)
+    setTitle(todo.title) //취소하면 원래대로 돌려놓기
   }
 
   return (
@@ -20,9 +34,17 @@ export default function TodoItem({ todo }: Props) {
       {isEditMode ? (
         //수정모드
         <>
-          <input type="checkbox" />
-          <input type="text" />
-          <button>취소</button>
+          <input
+            type="checkbox"
+            checked={todo.done}
+          />
+          <input
+            ref={inputRef} //참조걸기
+            type="text"
+            value={title} //todo.title로 하면 수정입력 안됨!
+            onChange={e => setTitle(e.target.value)}
+          />
+          <button onClick={() => offEditMode()}>취소</button>
           <button>저장</button>
           <button>삭제</button>
         </>
