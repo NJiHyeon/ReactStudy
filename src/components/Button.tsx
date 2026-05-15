90279f1ac2f4b034e406f2a7bef71ce9e14673d6
NJH <ggpji59105@naver.com>
2026-05-15 11:44:21 +0900

    feat(button): 로딩 버튼 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
+import Loader from '@/components/Loader'
+
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
+  loading?: boolean
 }
 
-export default function Button({ children, ...restProps }: Props) {
+export default function Button({
+  children,
+  loading = false, //디폴트 값 설정
+  ...restProps //나머지 요소들이 들어올 수도
+}: Props) {
   return (
     <button
-      className="h-[36px] min-w-[100px] cursor-pointer rounded-lg bg-blue-500 text-white duration-200 hover:bg-blue-400"
+      className="relative h-[36px] min-w-[100px] cursor-pointer rounded-lg bg-blue-500 text-white duration-200 hover:bg-blue-400"
       {...restProps}>
-      {children}
+      {loading ? <Loader color="white" /> : children}
     </button>
   )
 }
